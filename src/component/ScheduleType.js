417c8c2420a3d86319c9698b9cc88f7d68739527
chae33 <jungchae5812@sookmyung.ac.kr>
2024-04-18 18:00:43 +0900

    [add] page rendering
    create -> participate

--- src/component/ScheduleType.js
+++ src/component/ScheduleType.js
@@ -1,6 +1,9 @@
 import style from "../routes/basic.module.css";
+import { useNavigate } from "react-router-dom";
 
 function ScheduleType() {
+  const navigate = useNavigate();
+
   return (
     <div className={style.container}>
       <div className={style.text}>일정의 종류를 선택해주세요.</div>
@@ -9,7 +12,10 @@ function ScheduleType() {
         <button className={style.inputType}>요일별</button>
       </div>
       {/* react-calendar library 사용 */}
-      <button className={style.nextButton}>→</button>
+      <button className={style.nextButton}         
+      onClick ={() => { 
+          navigate("/:id"); 
+          }}>→</button>
     </div>
   );
 }
